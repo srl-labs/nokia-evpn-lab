8dea499c58e07c2c154d29a431eb2b7bb76a5854
bclasse <claeys.bastien@skynet.be>
2022-08-29 16:25:54 +0200

    Adding one more MH client, VPRN interfaces on DCGWs, update README

--- configs/eth1.sh
+++ configs/eth1.sh
@@ -1,14 +1,9 @@
 ip link add bond0 type bond
-ip link set bond0 type bond miimon 100 mode 802.3ad
+ip link set bond0 type bond miimon 100 mode balance-rr
 ip link set eth1 down
 ip link set eth2 down
-ip link set eth3 down
-ip link set eth4 down
 ip link set eth1 master bond0
 ip link set eth2 master bond0
-ip link set eth3 master bond0
-ip link set eth4 master bond0
 ip link set bond0 up
 ip link add link bond0 name bond0.1 type vlan id 1
 ip link set bond0.1 up
-ip addr add 192.168.1.10/24 dev bond0.1
